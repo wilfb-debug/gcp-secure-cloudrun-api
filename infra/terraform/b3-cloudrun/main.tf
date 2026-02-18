a376faba7dd5199db879434f590d32a50b45b8f2
Wilfred Ballo <wilfredballo@Wilfreds-MacBook-Pro.local>
2026-02-19 00:17:05 +0100

    feat: update Cloud Run ingress configuration (B5 prep)

--- infra/terraform/b3-cloudrun/main.tf
+++ infra/terraform/b3-cloudrun/main.tf
@@ -2,6 +2,8 @@ resource "google_cloud_run_v2_service" "api" {
   name     = var.service_name
   location = var.region
 
+  ingress = "INGRESS_TRAFFIC_INTERNAL_ONLY"
+
   template {
     service_account = var.cloudrun_sa_email
 
